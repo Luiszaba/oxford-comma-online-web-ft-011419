07626687bd0b21da40d815954b3e86320d820c16
Luis Zabala <luis.zabala076@gmail.com>
2019-01-14 22:31:53 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,3 @@
-def oxford_comma(array)
-  if array.length == 1
-    array.join
-    elsif array.length == 2
-    array.join(" and ")
-  else 
-    array.length == 3
-    array[-1].prepend "and "
-    array.join(", ")
-  end
-end
 
 def oxford_comma(array)
   if array.length == 1
@@ -17,6 +6,6 @@ def oxford_comma(array)
     array.join (" and ")
   else array.length == 3
     array.prepend "and"
-    array.join(" ")
+    array.join(" , ")
   end
 end
