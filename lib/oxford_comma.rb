89e32e3834a54842289bf41d2f2608fe6676c917
Luis Zabala <luis.zabala076@gmail.com>
2019-01-14 22:20:21 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,9 +4,7 @@ def oxford_comma(array)
     elsif array == 2
     array.join(",")
     elsif array == 3
-    array.prepend("and")
-    array[-1].join(",")
-    elsif array >= 3
+  else array[-1].prepend "and"
     
     
 
