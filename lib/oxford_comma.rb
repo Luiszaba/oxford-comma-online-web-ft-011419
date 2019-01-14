23b742d9b4948cabcf22ac65377bb82560ad8488
Luis Zabala <luis.zabala076@gmail.com>
2019-01-14 22:31:42 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -9,3 +9,14 @@ def oxford_comma(array)
     array.join(", ")
   end
 end
+
+def oxford_comma(array)
+  if array.length == 1
+    array.join
+    elsif array.length == 2
+    array.join (" and ")
+  else array.length == 3
+    array.prepend "and"
+    array.join(" ")
+  end
+end
